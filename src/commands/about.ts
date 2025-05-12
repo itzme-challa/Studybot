581910616245207659f222657d3026774299716e
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-12 22:19:54 +0530

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -6,19 +6,19 @@ import { author, name, version } from '../../package.json';
 const debug = createDebug('bot:about_command');
 
 const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}*\n\nAuthor: ${author}\n\n` +
-    `This bot is designed to provide helpful resources and tools for students preparing for NEET, JEE, and other competitive exams. ` +
-    `It includes a variety of study materials, practice tests, study groups, and more.\n\n` +
-    `Features include:\n` +
-    `- Access to study materials for NEET and JEE\n` +
-    `- Practice tests for NEET and JEE\n` +
-    `- Links to study groups for peer interaction\n` +
-    `- NCERT solutions and other helpful resources`;
+  const message = `*${name} ${version}*\n\n` +
+    `*Author:* ${author}\n\n` +
+    `This bot is designed to provide helpful resources and tools for students preparing for *NEET*, *JEE*, and other competitive exams\\.\n\n` +
+    `*Features include:*\n` +
+    `\\- Access to study materials for *NEET* and *JEE*\n` +
+    `\\- Practice tests for *NEET* and *JEE*\n` +
+    `\\- Links to study groups for peer interaction\n` +
+    `\\- NCERT solutions and other helpful resources\n\n` +
+    `You can also try [@EduhubKMR_bot](https://t.me/EduhubKMR_bot) \\– *EduhubKMR QuizBot* – Practice *NEET Biology*, *Physics* & *Chemistry* with answers and explanations\\! And more\\.`;
 
   debug(`Triggered "about" command with message \n${message}`);
 
-  // Send the message with additional details about the bot
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+  await ctx.replyWithMarkdownV2(message);
 };
 
 export { about };
