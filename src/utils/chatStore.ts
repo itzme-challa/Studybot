5a1f601846231abad1ea03936d464a113597a8ed
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-12 18:54:51 +0530

    Update chatStore.ts

--- src/utils/chatStore.ts
+++ src/utils/chatStore.ts
@@ -11,7 +11,7 @@ export const getAllChatIds = (): number[] => {
 };
 export const fetchChatIdsFromSheet = async (): Promise<number[]> => {
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbzHPhcv79YQyIx6t-59fsc6Czm9WgL6Y4HOP2JgX4gJyi3KjZqbXOGY-zmpyceW32VI/exec');
+    const response = await fetch('https://script.google.com/macros/s/AKfycbxm2Px6oTD8IZFnw6L3J82XJFthC7wEoknJvT_jutdm5ovHCwedxjaWbzI9XqTcSX-K/exec');
     const data = await response.json();
 
     const ids = data.map((entry: any) => Number(entry.id)).filter((id: number) => !isNaN(id));
