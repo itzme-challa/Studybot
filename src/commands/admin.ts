a9a43c748dccdc17ac25efe63467c2403c8e0116
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-14 07:15:49 +0530

    Update admin.ts

--- src/commands/admin.ts
+++ src/commands/admin.ts
@@ -1,6 +1,5 @@
 // src/commands/admin.ts
 import { Telegraf } from 'telegraf';
-import { CallbackQuery, CallbackQueryData } from 'telegraf/typings/core/types/typegram';
 import { fetchChatIdsFromSheet } from '../utils/chatStore';
 
 const ADMIN_ID = 6930703214;
@@ -28,7 +27,6 @@ export const setupAdminCommands = (bot: Telegraf) => {
   bot.on('callback_query', async (ctx) => {
     const callback = ctx.callbackQuery;
 
-    // Narrow to CallbackQuery.DataCallbackQuery
     if ('data' in callback && callback.data === 'refresh_users' && ctx.from?.id === ADMIN_ID) {
       try {
         const chatIds = await fetchChatIdsFromSheet();
