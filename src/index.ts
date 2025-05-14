f8861b6c5985135674925f7776108a36d3d3d5d5
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-14 12:40:42 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { greeting, checkMembership } from './text/greeting';
 import { production, development } from './core';
 import { isPrivateChat } from './utils/groupSettings';
 import { setupBroadcast } from './commands/broadcast';
-
+import { setupContactForwarding } from './commands/contact';
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 const ADMIN_ID = 6930703214;
@@ -54,6 +54,7 @@ bot.command('users', async (ctx) => {
 
 // Admin: /broadcast
 setupBroadcast(bot);
+setupContactForwarding(bot);
 
 // --- Callback Handler ---
 bot.on('callback_query', async (ctx) => {
